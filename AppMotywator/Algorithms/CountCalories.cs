4d889a9a9f2d17802efd52d1e9108db6a20fc111
Agata <agatagajec@gmail.com>
2017-11-11 18:10:27 +0100

    Creating a new class, which is made for counting calories

---
+++ AppMotywator/Algorithms/CountCalories.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppMotywator.Algorithms
+{
+    public class CountCalories
+    {
+        private int age;
+        private int height;     // cm
+        private int weight;     //kg
+        private int gender;
+        private int lifestyle;
+        private int physique;
+        private int target;
+
+        enum Gender
+        {
+            woman,
+            man
+        };
+
+        enum Lifestyle
+        {
+            lackofactivity,
+            sittingwork,
+            worknotphysical,
+            lightworkphysical,
+            workphysical,
+            heavyphysical
+        };
+
+        enum Physique
+        {
+            endomorfik,
+            mezomorfik,
+            ektomorfik
+        }
+
+
+        enum Aim
+        {
+            buildingmass,
+            weightreduction
+        }
+
+    }
+}
